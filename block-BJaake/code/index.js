83828a3ea7950e903d875fe65e5b06dc3a4f073c
Palacharla Narendra <palacharlanarendra@gmail.com>
2021-06-09 17:15:03 +0530

    few changes

--- block-BJaake/code/index.js
+++ block-BJaake/code/index.js
@@ -1,3 +1,5 @@
+let url = 'https://api.spaceflightnewsapi.net/v3/articles?_limit=30';
+let data = fetch(url).then((res) => res.json());
 let display = document.querySelector('.display');
 
 let source = document.querySelector('#source');
@@ -5,22 +7,19 @@ let source = document.querySelector('#source');
 function handleChange(event) {
   console.log(event);
   document.querySelector('.display').innerHTML = '';
-  let data2 = fetch('https://api.spaceflightnewsapi.net/v3/articles?_limit=30')
-    .then((res) => res.json())
-    .then((userData) =>
-      userData.forEach((data) => {
-        console.log(data.newsSite, event.target.value);
-        if (data.newsSite == event.target.value) {
-          createUI(data);
-        }
-      })
-    );
+
+  data.then((userData) =>
+    userData.forEach((data) => {
+      console.log(data.newsSite, event.target.value);
+      if (data.newsSite == event.target.value) {
+        createUI(data);
+      }
+    })
+  );
 }
 source.addEventListener('change', handleChange);
 
-let data = fetch('https://api.spaceflightnewsapi.net/v3/articles?_limit=30')
-  .then((res) => res.json())
-  .then((userData) => userData.forEach((data) => createUI(data)));
+data.then((userData) => userData.forEach((data) => createUI(data)));
 
 function createUI(arg) {
   let outer = document.createElement('div');
